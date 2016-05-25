0fb249c9f79f33b8fe0ebd424076f0aab85931a4
Nick Barrett <nick@juicebox.com.au>
2016-05-25 14:53:24 +0800

    moving some js to child theme

--- js/src/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-
-    jQuery('.hamburger').on('click',function(){
-        jQuery(this).toggleClass('is-active');
-        jQuery('.main-navigation').toggleClass('is-active');
-
-    });
-
-});
