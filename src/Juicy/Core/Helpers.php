33c57eaa09e501c75226d0c8d9997f0fe0c6db6f
Chris Manganaro <chrism@juicebox.com.au>
2016-09-01 10:46:15 +0800

    Add helper class

---
+++ src/Juicy/Core/Helpers.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * A class of helper functions that can be reused across sites.
+ */
+
+namespace Juicy\Core;
+
+class Helpers
+{
+    /**
+     * Are we in dev? Is WP_DEBUG set and does it equal true?
+     * @return boolean
+     */
+    public static function is_dev()
+    {
+        return (defined('WP_DEBUG') && WP_DEBUG === true);
+    }
+
+    /**
+     * Get site options by a prefix
+     * @param  string $prefix the prefix of the option, e.g. error
+     * @return array         An associative array of option, with the prefix removed
+     */
+    public static function option_by_prefix( $prefix )
+    {
+        // Get all options
+        $fields = get_fields('option');
+        // Init array
+        $return = array();
+        // Loop through each option, we will test the key against the prefix passed in
+        foreach ( $fields as $key => $option ) {
+            // If the first X letters of the key are equal to the prefix we have an option
+            if ( substr($key, 0, strlen($prefix)) === $prefix ) {
+                // remove the prefix, we don't need it anymore
+                $return[str_replace($prefix.'_', '', $key)] = $option;
+            }
+
+        }
+
+        return $return;
+    }
+}
--- src/Juicy/Core/Image.php
+++ src/Juicy/Core/Image.php
@@ -13,7 +13,8 @@ class Image extends TimberImage {
     /**
      * @param int $iid
      */
-    public function __construct($iid) {
+    public function __construct($iid)
+    {
         $this->iid = $iid;
 
         parent::__construct($iid);
@@ -23,7 +24,8 @@ class Image extends TimberImage {
      * @param string|array $size - See http://codex.wordpress.org/Function_Reference/wp_get_attachment_image_src
      * @return array|bool|string
      */
-    public function src($size = 'full') {
+    public function src($size = 'full')
+    {
         $src = wp_get_attachment_image_src($this->iid, $size);
         return $src[0];
     }
@@ -33,7 +35,27 @@ class Image extends TimberImage {
      * @param string|array $size - See http://codex.wordpress.org/Function_Reference/wp_get_attachment_image_src
      * @return array|bool|string
      */
-    public function get_src($size = 'full') {
+    public function get_src($size = 'full')
+    {
         return $this->src($size);
+    }
+
+    public function alt()
+    {
+        $alt = parent::alt();
+        // Check if alt is set
+        if ( $alt != '' ) {
+            return $alt;
+        }
+        // Otherwise we check for image title
+        $alt = parent::title;
+
+        if ( $alt != '' ) {
+            return $alt;
+        }
+        // Lastly fall back to post title
+        global $post;
+        // post_title because global $post will be WP_Post
+        return $post->post_title;
     }
 }
