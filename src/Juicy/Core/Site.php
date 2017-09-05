77133d8f9eb64150ef382bba3a869e4028df0cf1
Chris Manganaro <chrism@juicebox.com.au>
2017-09-05 08:46:11 +0800

    theme option shortcode and updat page content

--- src/Juicy/Core/Site.php
+++ src/Juicy/Core/Site.php
@@ -213,16 +213,28 @@ class Site extends TimberSite
     {
         $this->create_page_if_null('Typography');
         $this->create_page_if_null('Grid');
-        $this->create_page_if_null('Home Page', 'publish', 'page--home-page.php');
-        $this->create_page_if_null('Contact', 'publish', 'page--contact.php');
+        $this->create_page_if_null('Home Page', '', 'publish', 'page--home-page.php');
+        $this->create_page_if_null('Contact', '', 'publish', 'page--contact.php');
+
+        $dir = new \DirectoryIterator(get_template_directory() . '/pages');
+        
+        foreach ($dir as $dirinfo) {
+
+            if (!$dirinfo->isDot()) {
+                $this->create_page_if_null(
+                    $dirinfo->getBasename(".{$dirinfo->getExtension()}"),
+                    file_get_contents($dirinfo->getPathname())
+                );
+            }
+        }
     }
 
-    public function create_page_if_null($title, $status = 'draft', $template = null)
+    public function create_page_if_null($title, $content = '', $status = 'draft', $template = null)
     {
         if (get_page_by_title($title) == NULL) {
             $page = array(
                 'post_title' => $title,
-                'post_content' => '',
+                'post_content' => $content,
                 'post_status' => $status,
                 'post_author' => 1,
                 'post_type' => 'page',
